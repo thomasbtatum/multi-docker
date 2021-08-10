af47e8da152cf4f13451d93e6d7310c1a747fe33
Thomas Tatum <thomas.b.tatum.ctr@nga.mil>
2021-08-10 14:44:11 -0400

    small updates

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React homie
+            Fib Calculator
           </a>
         
         <Link to="/">Home</Link>
